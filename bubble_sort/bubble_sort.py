3a3da257617d8a0adb9e11f9d83ab3430bbf5b54
Seyia07 <seyiasankok@gmail.com>
2025-06-20 23:22:54 +0300

    Implemented selection sort and bubble sort...assignment

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,11 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    n = len(unsorted_list)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                temp = unsorted_list[j]
+                unsorted_list[j] = unsorted_list[j + 1]
+                unsorted_list[j + 1] = temp
+
+    return unsorted_list
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,11 @@
-def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+def bubble_sort(unsorted_list):
+    n = len(unsorted_list)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                temp = unsorted_list[j]
+                unsorted_list[j] = unsorted_list[j + 1]
+                unsorted_list[j + 1] = temp
+
+    return unsorted_list
