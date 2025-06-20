d9fdb65d29d12f0de08671cc97de416bb9cf722d
Seyia07 <seyiasankok@gmail.com>
2025-06-20 23:29:58 +0300

    Corrected selection_sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,11 +1,16 @@
-def bubble_sort(unsorted_list):
+def selection_sort(unsorted_list):
     n = len(unsorted_list)
 
     for i in range(n):
-        for j in range(n - i - 1):
-            if unsorted_list[j] > unsorted_list[j + 1]:
-                temp = unsorted_list[j]
-                unsorted_list[j] = unsorted_list[j + 1]
-                unsorted_list[j + 1] = temp
+        min_index = i
+
+        for j in range(i + 1, n):
+            if unsorted_list[j] < unsorted_list[min_index]:
+                min_index = j
+
+        # swap
+        temp = unsorted_list[i]
+        unsorted_list[i] = unsorted_list[min_index]
+        unsorted_list[min_index] = temp
 
     return unsorted_list
